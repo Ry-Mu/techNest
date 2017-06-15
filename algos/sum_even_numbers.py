2a0e60d74a453d3d9c4a257589e9c3a1090d9044
Ryan Munguia <RyanMunguia@MacBook-Pro-5.local>
2017-06-14 19:25:57 -0700

    adding sum even number file

---
+++ algos/sum_even_numbers.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jun 14 19:02:13 2017
+
+@author: RyanMunguia
+"""
+
+
+def sum_even_num():
+    sum = 0
+    for i in range(1,1001):
+        if i % 2 == 0:
+            sum += i
+    print(sum)
+
+sum_even_num()
+
