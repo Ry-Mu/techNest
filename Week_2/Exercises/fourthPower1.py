42bb596ee4eea94507c364d23c4b6713acbe856e
Ryan Munguia <ryan.munguia92@gmail.com>
2017-06-21 19:40:58 -0700

    adding swap val algo

---
+++ Week_2/Exercises/fourthPower1.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jun 21 19:30:31 2017
+
+@author: RyanMunguia
+"""
+
+#Write a Python function, fourthPower, that takes in one number and returns that
+#value raised to the fourth power.
+
+#You should use the square procedure that you defined in an earlier exercise 
+#(you don't need to redefine square in this box; when you call square, the grader will use our definition).
+
+#This function takes in one number and returns one number.
+
+def fourthPower(x):
+    return x**4
+
+print(fourthPower(2))
+
+    
